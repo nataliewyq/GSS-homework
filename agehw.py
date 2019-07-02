1fb5f3af8f0a5b7e6ef5ef7f51b2078a9e77e6ec
nataliewyq <52395345+nataliewyq@users.noreply.github.com>
2019-07-02 21:36:09 +0800

    Add files via upload

---
+++ agehw.py
@@ -0,0 +1,10 @@
+print("enter your age")
+age=int(input(""))
+a=age*12
+print("the number of months you have lived:", a)
+b=age*365
+print("the number of days you have lived:", b)
+c=age*8760
+print("the number of hours you have lived:", c)
+d=age*525600
+print("the number of minutes you have lived:", d)
